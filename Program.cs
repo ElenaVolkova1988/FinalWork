309bd9ee18f604bc8b65c7e5db1c347957b78e13
Замглавбух <@dpsbux.local>
2022-11-25 15:08:42 +0300

    show tree-charaster elements

--- Program.cs
+++ Program.cs
@@ -1,25 +1,40 @@
 ﻿// Написать программу, котрая из имеющегося массива строк формирует массив из строк, длина которых меньше или равна 3 символа.
 string [ ] CreateArray (int size)
 {
-    
-    string [] myArray=new string [size];
-
-    for (int i= 0; i<myArray.Length; i++)
-    {
-        Console.WriteLine("Input information :");
-        myArray[i]=Console.ReadLine();
-    }
-    return myArray;
+  string [] array = new string[size];
+      for(int i=0; i< size ; i++)
+{
+      Console.Write($"Input a {i+1} element of array: ");
+      array [i]= Convert.ToString(Console.ReadLine());
 }
+         return array;
+} 
 void ShowArray (string []array) 
  {
     for( int i=0; i< array.Length; i++)
     Console.Write(array[i]+ " ");
     Console.WriteLine();
  }
+
+string [] ThreeCharacters ( string [] array)
+{
+    string [] finalArray = new string [array.Length];
+    int count = 0;
+    for (int i = 0; i < finalArray.Length; i++)
+    {
+    if(array[i].Length <= 3)
+        {
+        finalArray[count] = array[i];
+        count++;
+        }
+    }
+    return finalArray;
+}
+
 Console.WriteLine("Input size array :");
 int size= Convert.ToInt32(Console.ReadLine());
 string [] myArray = CreateArray(size);
 ShowArray(myArray);
-
+string [] newArray=ThreeCharacters(myArray);
+ShowArray(newArray);
 
